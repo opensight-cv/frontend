e6237abc2d370b657472fa0a96cdf89cb063fc01
g10ria <22gloriaz@students.harker.org>
2020-04-26 17:55:20 -0700

    Move into subdirectory + rename options

--- src/components/baklava-options-plugin/OptionPlugin.ts
+++ src/components/baklava-options-plugin/OptionPlugin.ts
@@ -7,28 +7,28 @@ import { OptionPlugin } from "@baklavajs/plugin-options-vue";
 import { IViewPlugin } from "@baklavajs/plugin-renderer-vue/dist/baklavajs-plugin-renderer-vue/types";
 import { IEditor } from "@baklavajs/plugin-renderer-vue/dist/baklavajs-core/types";
 
-import * as Options from "./Options";
+import * as Options from "./options";
 
 /**
  * Copypasta from the original OptionPlugin class
- * Modified to use our custom options list (OpsiOptions)
+ * Modified to use our custom options list
  */
 
 export default class extends OptionPlugin {
     public register(editor: IEditor) {
         editor.events.usePlugin.addListener(this, (p) => {
             if (p.type === "ViewPlugin") {
-                this.OpsiRegisterOptions(p as IViewPlugin); // modified
+                this.opsiRegisterOptions(p as IViewPlugin); // modified
             }
         });
         editor.plugins.forEach((p) => {
             if (p.type === "ViewPlugin") {
-                this.OpsiRegisterOptions(p as IViewPlugin); // modified
+                this.opsiRegisterOptions(p as IViewPlugin); // modified
             }
         });
     }
 
-    private OpsiRegisterOptions(viewPlugin: IViewPlugin) {
+    private opsiRegisterOptions(viewPlugin: IViewPlugin) {
         Object.entries(Options).forEach(([k, v]) => {
             viewPlugin.registerOption(k, v);
         });
---
+++ src/components/baklava-options-plugin/options.ts
@@ -0,0 +1,24 @@
+import {
+  ButtonOption,
+  CheckboxOption,
+  InputOption,
+  IntegerOption,
+  NumberOption,
+  SelectOption,
+  SliderOption,
+  TextOption,
+} from "@baklavajs/plugin-options-vue";
+
+import DragNumberOption from "./DragNumberOption.vue";
+
+export {
+  ButtonOption,
+  CheckboxOption,
+  InputOption,
+  IntegerOption,
+  NumberOption,
+  SelectOption,
+  SliderOption,
+  TextOption,
+  DragNumberOption,
+};

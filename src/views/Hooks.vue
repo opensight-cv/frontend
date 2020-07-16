6cf0bacc6e3151e4bdaf7d325825fbd7259d0f7d
Chirag Kaushik <22chiragk@students.harker.org>
2020-07-16 15:51:45 -0700

    Hooks structure

--- src/views/Hooks.vue
+++ src/views/Hooks.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="hooks">
-    Hooks
+    <pref-node title="Hooks">
+      <ul>
+        <li>opsi.video</li>
+      </ul>
+    </pref-node>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend();
+import PrefNode from "@/components/ui/PrefNode.vue";
+
+export default Vue.extend({
+  components: { PrefNode },
+});
 </script>
 
 <style scoped>
 .hooks {
   background-color: #232323;
   color: white;
+
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-content: flex-start;
+
+  padding: 2% 2%;
+  box-sizing: border-box;
+}
+
+.settings > div {
+  margin-right: 2%;
+  margin-bottom: 2%;
 }
 </style>
